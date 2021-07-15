c5a36426f331209da2f46373e83fa8d4d9f3868c
Ahmed525-12 <ahmedsayed5250057@gmail.com>
2021-07-15 06:33:42 +0300

    first commit

---
+++ 1401-image-hover/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/littlesnippets/pen/dGLbXY.
+
+ Image with title, caption & icon on hover.
---
+++ script.js
@@ -0,0 +1,68 @@
+// -------------------------------------------------navbar-----------------------------
+
+
+$(window).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+// -------------------------------------------------navbar-----------------------------
+
+// -------------------------------------------------- header ----------------------------
+
+
+var typed = new Typed('.element', {
+    strings: ['','AHMED SAYED','WEB DEVELOPER','FRONT-END',],
+    smartBackspace: true ,
+    typeSpeed: 100,
+    loop:true,
+    backSpeed: 50,
+    backDelay: 500,
+  });
+
+
+// -------------------------------------------------- header ----------------------------
+
+// --------------------------------------------------up btn --------------------------------------------------------
+$(window).scroll(function () {
+    $('a').toggleClass('scrollh', $(this).scrollTop() > 50);
+});
+
+$(".demoupbtn").click(function(){
+    $("body,html").animate({scrollTop:'0'},1000)
+})
+
+// --------------------------------------------------up btn --------------------------------------------------------
+$("a[href^='#']").click(function(e) {
+    let ax = $(e.target).attr("href");
+    let ay = $(ax).offset().top;
+    $("body,html").animate({scrollTop:ay},1000)
+})
+
+// --------------------------------------------------up btn --------------------------------------------------------
+
+$(document).ready(function(){
+    
+    $("#store-container").fadeOut(3000,function(){
+        $("#loading").fadeOut(500,function(){
+          
+            $("#loading").remove()
+        })
+    })
+})
+
+// -----------------------------------------------------------------------------------------------------
+
+$('body').scrollspy({target: '.navbar-fixed-top'})
+
+// ---------------------------------------------------------------------------------------------------------
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 9000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
